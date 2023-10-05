cb36bc7e07d8533c772837e8fc3a51c1d37455b6
Rodrigo <Rodrigo@HP_RODRIGO>
2023-10-04 20:08:38 -0500

    Agregar archivos de proyecto.

---
+++ lab07_01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace lab07_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, limite;
+            float resultado, total;
+            resultado = 0f;
+            total = 0f;
+            Console.Write("Ingrese el valor de n: ");
+            limite = int.Parse(Console.ReadLine());
+
+            for (i=1; i<=limite; i++)
+            {
+                resultado = (16f - i) / (3f * i);
+                Console.WriteLine("resultado: " + resultado);
+                total = total + resultado; 
+            }
+            Console.WriteLine("total: " + total);
+            Console.ReadKey();
+        }
+    }
+}
